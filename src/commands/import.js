e62c58b3abf7b1f4f132b6df3650469686dfb75b
hxw319 <40411224+hxw319@users.noreply.github.com>
2022-09-22 19:23:57 +0800

    Export files

--- src/commands/import.js
+++ src/commands/import.js
@@ -1,7 +1,9 @@
 const Figma = require('figma-api');
+const fs = require('fs');
 const {buildTagTree, buildCode, buildCssString} = require('figma-tree-parser');
 const findExportedNode = require('../utils/findExportedNode');
 const transformNode = require('../utils/transformNode');
+const { stringify } = require('querystring');
 
 const importFigma = async (token, fileKey, exportTo) => {
     const api = new Figma.Api({
@@ -14,11 +16,18 @@ const importFigma = async (token, fileKey, exportTo) => {
         transformNode(n, file.components);
         const tag = buildTagTree(n);
         const codeStr = buildCode(tag, 'css');
+        const codeStrFile = 'codeStr.txs'
+        fs.writeFile(codeStrFile, codeStr, (err) => {
+            if(err) throw err
+        })
+        
         const cssStr = buildCssString(tag, 'css');
-        console.log(codeStr);
-        console.log(cssStr);
-        //todo
-        //export to file
+        const cssStrFile = 'cssStr.txs'
+        fs.writeFile(cssStrFile, cssStr, (err) => {
+            if(err) throw err
+        })
+        // console.log(codeStr);
+        // console.log(cssStr);
     })
     return file;
 }
